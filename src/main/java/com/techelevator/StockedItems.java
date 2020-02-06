d0b58ccea0bda17b37cd00ebd40d527c0a9c3d7d
Leigh Hartmann <lahartmann@gmail.com>
2020-02-06 12:42:39 -0500

    first class created capstone 1

---
+++ src/main/java/com/techelevator/StockedItems.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+public class StockedItems {
+
+	private String name;
+	private double price;
+	
+	public StockedItems(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+}
