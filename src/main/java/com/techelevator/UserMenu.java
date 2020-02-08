f427ae7184c63f4454f6b1581c16297fc4a6a8d5
Irina Tin <irina.tin@me.com>
2020-02-07 22:25:21 -0500

    Working on audit

--- src/main/java/com/techelevator/UserMenu.java
+++ src/main/java/com/techelevator/UserMenu.java
@@ -2,6 +2,11 @@ package com.techelevator;
 
 import java.awt.DisplayMode;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Map;
@@ -13,81 +18,55 @@ import javax.swing.text.Keymap;
 
 public class UserMenu {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		Scanner userInput = new Scanner(System.in);
 
 		VendingMachine ourMachine = new VendingMachine();
 
 		String option = "";
-		
-		String optionForPurchaseMenu = "";
-		
-		BigDecimal customerMoneyEntered = new BigDecimal(0);
-		
 
-		while (!option.equals("3")) { //while Exit is not chosen in the Main Menu
-			ourMachine.runMainMenu(); //prints out the Main Menu
-			System.out.println("Please select one of the options: "); //asks for selection
-			option = userInput.nextLine(); //stores the selection
-			if (option.equals("1")) { //if selection is 1
+		String optionForPurchaseMenu = "";
+
+		BigDecimal customerMoneyEntered = new BigDecimal(0);
+
+		while (!option.equals("3")) { // while Exit is not chosen in the Main Menu
+			ourMachine.runMainMenu(); // prints out the Main Menu
+			System.out.println("Please select one of the options: "); // asks for selection
+			option = userInput.nextLine(); // stores the selection
+			if (option.equals("1")) { // if selection is 1
 				ourMachine.displayMenuItems(); // prints out the Item Menu (Inventory)
-			} else if(option.equals("2")){
-				
+			} else if (option.equals("2")) {
+
 				while (true) { // while Exit is not chosen in the Item Menu
 					ourMachine.displayPurchaseMenu(); // prints out purchase menu
 					System.out.println("Please select one of the options: ");
-					
-					optionForPurchaseMenu = userInput.nextLine(); //stores the option
-					if(optionForPurchaseMenu.equals("1")) { //if 1
-						ourMachine.feedMoney();//ask for amount to input
-						customerMoneyEntered = new BigDecimal(userInput.nextLine());//amount stored as BigDecimal
-						ourMachine.setBalance(customerMoneyEntered);//updates the machine balance
+
+					optionForPurchaseMenu = userInput.nextLine(); // stores the option
+					if (optionForPurchaseMenu.equals("1")) { // if 1
+						ourMachine.feedMoney();// ask for amount to input
+						customerMoneyEntered = new BigDecimal(userInput.nextLine());// amount stored as BigDecimal
+						ourMachine.setBalance(customerMoneyEntered);// updates the machine balance
+						//record the transaction in log.txt
 						
-						
-					} else if(optionForPurchaseMenu.equals("2")) {
+
+					} else if (optionForPurchaseMenu.equals("2")) {
 						ourMachine.displayMenuItems();
 						System.out.println("Please select an item to purchase: ");
-						String optionOfItemChosen = userInput.nextLine().toUpperCase(); //slot #
+						String optionOfItemChosen = userInput.nextLine().toUpperCase(); // slot #
 						ourMachine.purchaseMethod(optionOfItemChosen);
-						
+
 					} else {
 						ourMachine.dispenseTheChange();
+						//record the transaction in log.txt
 						break;
-						
+
 					}
-						
-					}
-				} 
+
+				}
 			}
 		}
 
-		// ourMachine.runMainManu();
-
-//		System.out.println("Would you like to make a purchase? Y/N ");
-//		String optionToPurchaseOrNot = userInput.nextLine();
-//		
-//		if(optionToPurchaseOrNot.equalsIgnoreCase("Y")) {
-//			System.out.println("Please input the money");
-//			ourMachine.displayPurchaseMenu();
-//			String inputMoney = userInput.nextLine();
-//			while(!inputMoney.equals("1")) {
-//				System.out.println("Sorry, your credit balance is $0.00. Please input some money.");
-//				ourMachine.displayPurchaseMenu();
-//			} 
-
-//		} else {
-//			System.out.println("Have a great day!");
-//			System.exit(1);
-//		}
-
-//		System.out.println("Please choose an item: ");
-//		String itemSlotForPurchase = userInput.nextLine();
-
-		// Scanner newScanner = new Scanner();
-
-		// ourMachine.displayMenuItems();
-
 	}
 
-
+}
--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,41 +1,41 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.Map;
 
 public class VendingMachine {
-	
-	
+
 	private BigDecimal customerBalance = new BigDecimal(0);
-	
+
 	private BigDecimal customerMoneyEntered = new BigDecimal(0);
-	
+
 	private BigDecimal machineBalance = new BigDecimal(0);
-	
+
 	private final double QUARTER = .25;
-	
+
 	private final double NICKEL = .05;
-	
+
 	private final double DIME = .10;
-	
+
 	private int numOfQuarters = 0;
-	
+
 	private int numOfNickels = 0;
-	
+
 	private int numOfDimes = 0;
-	
+
 	private BigDecimal change = new BigDecimal(0);
-	
+
 //	private String changeSplit = "";
-	
-	
+
 	Inventory newInventory = new Inventory();
 
 	File sourceFile = newInventory.getInputFile();
 
 	Map<String, StockedItems> itemMap = newInventory.stockInventory(sourceFile);
-	
 
 	public void runMainMenu() {
 
@@ -44,8 +44,6 @@ public class VendingMachine {
 	}
 
 	public void displayMenuItems() {
-
-		
 
 		for (String key : itemMap.keySet()) {
 			if (itemMap.get(key).getQuantity() > 0) {
@@ -56,20 +54,21 @@ public class VendingMachine {
 			}
 		}
 	}
-	
+
 	public void purchaseMethod(String key) {
-		if(!itemMap.containsKey(key)) {
+		if (!itemMap.containsKey(key)) {
 			System.out.println("Sorry, that slot doesn't exist!");
-		} else if(itemMap.get(key).getQuantity() == 0) {
+		} else if (itemMap.get(key).getQuantity() == 0) {
 			System.out.println("SOLD OUT");
-		} else if(machineBalance.compareTo(itemMap.get(key).getPrice()) == -1) { //if balance is less than purchase price
+		} else if (machineBalance.compareTo(itemMap.get(key).getPrice()) == -1) { // if balance is less than purchase
+																					// price
 			System.out.println("Please, add more money!");
-		} else if(machineBalance.compareTo(itemMap.get(key).getPrice()) >= 0) {
+		} else if (machineBalance.compareTo(itemMap.get(key).getPrice()) >= 0) {
 			System.out.println("Enjoy your " + itemMap.get(key).getName() + "! " + itemMap.get(key).getSound());
 			machineBalance = machineBalance.subtract(itemMap.get(key).getPrice());
 			itemMap.get(key).itemPurchased();
 		}
-	
+
 	}
 
 	public void displayPurchaseMenu() {
@@ -81,36 +80,67 @@ public class VendingMachine {
 	public void feedMoney() {
 		System.out.println("Please enter the amount: ");
 	}
-	
+
 	public BigDecimal getCustomerMoneyEntered() {
 		return customerMoneyEntered;
 	}
 
-	//vending machine balance after the application is open. Has a $0.00 balance by default
+	// vending machine balance after the application is open. Has a $0.00 balance by
+	// default
 	public BigDecimal getBalance() {
 		return machineBalance;
 	}
 
-	//UPDATED BALANCE AFTER CUSTOMER'S MONEY INPUT
+	// UPDATED BALANCE AFTER CUSTOMER'S MONEY INPUT
 	public void setBalance(BigDecimal customerMoneyEntered) {
 		this.machineBalance = machineBalance.add(customerMoneyEntered);
 	}
-	
-	//Updated balance after purchase
+
+	// Updated balance after purchase
 	public void balanceAfterPurchase(BigDecimal itemPrice) {
 		this.machineBalance = customerBalance.subtract(itemPrice);
 	}
-	
-	//dispense the change
+
+	// dispense the change
 	public void dispenseTheChange() {
-		numOfQuarters = (int) (this.machineBalance.doubleValue()/QUARTER);
+		numOfQuarters = (int) (this.machineBalance.doubleValue() / QUARTER);
 		double newBalance = this.machineBalance.doubleValue() % QUARTER;
-			numOfDimes = (int)(newBalance/DIME);
-			newBalance = newBalance % DIME;
-				numOfNickels = (int)(newBalance/NICKEL);
-			
-			System.out.println ("Your change is: " + this.machineBalance + "\n" + numOfQuarters + ": Quarters\n" + numOfDimes + ": Dimes\n" + numOfNickels + ": Nickels" );
-		}
+		numOfDimes = (int) (newBalance / DIME);
+		newBalance = newBalance % DIME;
+		numOfNickels = (int) (newBalance / NICKEL);
+
+		System.out.println("Your change is: " + this.machineBalance + "\n" + numOfQuarters + ": Quarters\n" + numOfDimes
+				+ ": Dimes\n" + numOfNickels + ": Nickels");
 	}
 
+//	public void logFeedMoney() {
+//		String fileName = "Log.txt";
+//		File newFile = new File(fileName);
+//
+//		try (FileWriter fileWriter = new FileWriter(newFile, true)) {
+//			PrintWriter writer = new PrintWriter(fileWriter);
+//
+//			writer.println("FEED MONEY: $" + customerMoneyEntered + machineBalance);
+//
+//		} catch (IOException e) {
+//			System.out.println("File not found");
+//		}
+//
+//	}
+//
+//	public void logPurchaseProduct() {
+//		String fileName = "Log.txt";
+//		File newFile = new File(fileName);
+//
+//		try (FileWriter fileWriter = new FileWriter(newFile, true)) {
+//			PrintWriter writer = new PrintWriter(fileWriter);
+//
+//			writer.println("GIVE CHANGE: $" + machineBalance + " $" + /*itemMap.get(key).getPrice());*/
+//
+//		} catch (IOException e) {
+//			System.out.println("File not found");
+//		}
+//
+//	}
 
+}
